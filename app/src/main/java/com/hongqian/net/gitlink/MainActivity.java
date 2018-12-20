d4a50f6c8eb84c5a6f8846a0e5e7c5fc73f5287c
hongqian.better <hongqian.better@outlook.com>
2018-12-20 11:46:56 +0800

    :sparkles:新建mycoin分支,积分商城开发

--- app/src/main/java/com/hongqian/net/gitlink/MainActivity.java
+++ app/src/main/java/com/hongqian/net/gitlink/MainActivity.java
@@ -2,6 +2,7 @@ package com.hongqian.net.gitlink;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("feature/mycoin","第一次修改");
     }
 }
